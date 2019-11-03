060c2aa6b6311c033d9b7ccd5f4f6cd3ca86ff5a
nem0 <rrivera@haverford.edu>
2019-11-02 23:58:06 -0400

    reorganized some files

--- code/schedule.py
+++ code/schedule.py
@@ -1,51 +1,5 @@
 from classes import *
 from obj_init import *
-# import heap
-
-def schedule(constraints, student_pref, output_file):
-	# constraints = 'constraints.txt'
-	# student_pref = 'student_prefs.txt'
-	# output_file = 'schedule.out'
-
-	Rooms, Timeslots, Students, Courses, Professors = build_all_objs(constraints, student_pref)
-
-	'''
-	Rooms is a list of ROOM OBJECTS sorted by size
-	Timeslots is a list of TIMESLOT OBJECTS that is SORTED by name (1..n)
-	Students is a list of STUDENT OBJECTS sorted by name (1..n)
-	Courses is a list of COURSE OBJECTS sorted by name (1..n)
-	Professors is a list of PROFESSOR OBJECTS sorted by popularity,
-		where popularity is the sum of the students that want
-		to enroll in both of that professor's classes.
-	'''
-
-	for professor in Professors:
-		#assign timeslots and rooms for both classes
-		assignSlot(professor.cl_1, None, Timeslots, Courses, Students)
-
-		assignSlot(professor.cl_2, professor.cl_1.time, Timeslots, Courses, Students)
-
-	#format output
-	output = []
-	for i in range(len(Courses)+1):
-		if i==0:
-			header_str="Course\tRoom\tTeacher\tTime\tStudents"
-			output.append(header_str)
-		else:
-			output.append(str(Courses[i-1].name))
-			output[i] += "\t" + str(Courses[i-1].room.name)
-			output[i] += "\t" + str(Courses[i-1].prof.name)
-			output[i] += "\t" + str(Courses[i-1].time.name) + "\t"
-			for j in range(Courses[i-1].enrollment):
-				output[i] += str(Courses[i-1].students[j]) + " "
-			
-	outString = ""
-	for i in range(len(output)):
-		outString += output[i] + "\n"
-
-	f = open(output_file,"w+")
-	f.write(outString)
-	f.close()
 
 # assigns a course to a timeslot and a room, enrolls students in the course
 def assignSlot(cl, badTime, Timeslots, Courses, Students):
@@ -95,4 +49,48 @@ def checkCompatibility(time, cl):
 		if not time.students[student - 1]:
 			compatibility += 1
 			compatibleStuds.append(student)
-	return compatibility,compatibleStuds
+	return compatibility, compatibleStuds
+
+
+# main scheduling algorithm 
+def schedule(constraints, student_pref, output_file):
+
+	'''
+	Rooms is a list of ROOM OBJECTS sorted by size
+	Timeslots is a list of TIMESLOT OBJECTS that is SORTED by name (1..n)
+	Students is a list of STUDENT OBJECTS sorted by name (1..n)
+	Courses is a list of COURSE OBJECTS sorted by name (1..n)
+	Professors is a list of PROFESSOR OBJECTS sorted by popularity,
+		where popularity is the sum of the students that want
+		to enroll in both of that professor's classes.
+	'''
+
+	Rooms, Timeslots, Students, Courses, Professors = build_all_objs(constraints, student_pref)
+	
+	for professor in Professors:
+		#assign timeslots and rooms for both classes
+		assignSlot(professor.cl_1, None, Timeslots, Courses, Students)
+		assignSlot(professor.cl_2, professor.cl_1.time, Timeslots, Courses, Students)
+
+	#format output
+	output = []
+	for i in range(len(Courses) + 1):
+		if i == 0:
+			header_str="Course\tRoom\tTeacher\tTime\tStudents"
+			output.append(header_str)
+		else:
+			output.append(str(Courses[i - 1].name))
+			output[i] += "\t" + str(Courses[i - 1].room.name)
+			output[i] += "\t" + str(Courses[i - 1].prof.name)
+			output[i] += "\t" + str(Courses[i - 1].time.name) + "\t"
+			for j in range(Courses[i - 1].enrollment):
+				output[i] += str(Courses[i - 1].students[j]) + " "
+			
+	outString = ""
+	for i in range(len(output)):
+		outString += output[i] + "\n"
+
+	f = open(output_file,"w+")
+	f.write(outString)
+	f.close()
+
